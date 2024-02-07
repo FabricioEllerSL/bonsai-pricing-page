9f88940d5842fabbaef02d501037ed82d6e1ae46
FabricioEllerSL <fabricioellersl@gmail.com>
2024-02-07 13:50:47 -0300

    feat: plans n pricing title and switch

--- front-end/src/components/Bonsai.vue
+++ front-end/src/components/Bonsai.vue
@@ -13,10 +13,13 @@
 
 <template>
   <v-container fluid class="">
+
+    <!-- NAVBAR / HEADER -->
+
     <v-app-bar :elevation="0">
       <v-row>
         <v-col class="text-center">
-          <h1 class="text-primary">bonsai</h1>
+          <h1 class="text-primary ml-n16">bonsai</h1>
         </v-col>
         <v-col class="d-flex justify-space-around align-center">
           <!-- DROP DOWN MENUS -->
@@ -51,11 +54,100 @@
         </v-col>
       </v-row>
     </v-app-bar>
+
+    <!-- PLANS AND PRICING -->
+
+    <v-container class="mt-16 px-16">
+      <v-row>
+        <v-col>
+          <h2 class="text-h4 my-font-serif">Plans & Pricing</h2>
+        </v-col>
+        <v-col>
+          <h5 class="text-end mb-3 text-primary">2 MONTHS FREE!</h5>
+          <div class="d-flex align-center justify-end">
+            <h3 class="font-weight-regular">MONTHLY</h3>
+            <label class="switch mx-5">
+              <input type="checkbox">
+              <span class="slider round"></span>
+            </label>
+            <h3 class="text-grey font-weight-regular">YEARLY</h3>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-container>
 </template>
 
 <style>
 .log-in-btn{
   border: 3px solid #00b289;
+}
+
+.my-font-serif{
+  font-family: serif;
+}
+
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #00b289;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #00b289;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #00b289;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
 }
 </style>
